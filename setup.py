0dc885fa8a40445180340bae9a0b0ce805bd1384
Your Name <you@example.com>
2024-04-18 16:22:28 +0200

    n

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="PyTS",
+    name="TimeSeriesPy",
     version="0.0.1",
     author="Fabio Scielzo Ortiz",
     author_email="fabioscielzo98@gmail.com",
     description="A package for time series forecasting.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/FabioScielzoOrtiz/PyTS_Package",  # add your project URL here
+    url="https://github.com/FabioScielzoOrtiz/TimeSeriesPy_Package",  # add your project URL here
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
